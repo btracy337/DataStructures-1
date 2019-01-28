4da1635993d0e5069a94f8e091c5a0dd64cf4e16
Guillermo <memo33101@gmail.com>
2019-01-28 09:06:30 -0700

    Created runner and controller files

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 1/28/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller:: start()
+{
+    cout << "welcome to data structures app" << endl;
+}
---
+++ DataStructures/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 1/28/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+class Controller
+{
+public:
+    void start();
+};
+
+#endif /* Controller_hpp */
