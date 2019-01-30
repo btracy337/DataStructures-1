a155486102045c99bf8ad0732ed32767c6b5e8a7
Guillermo <memo33101@gmail.com>
2019-01-30 09:37:48 -0700

    Created Array hpp file

---
+++ DataStructures/Model/Linear/Array.hpp
@@ -0,0 +1,44 @@
+//
+//  Array.hpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 1/30/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+template <class Type>
+class Array
+{
+private:
+    Type * internalArray;
+    int size;
+
+public:
+    //constructor
+    Array<Type>(int size);
+    
+    //copy constructor
+    Array<Type>(const Array<Type> & toCopy);
+    //destructor
+    ~Array<Type>();
+    
+    //operators
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    //methods
+    int getSize() const;
+    Type getFromIndex(int index)
+    
+};
+
+
+#endif /* Array_h */
