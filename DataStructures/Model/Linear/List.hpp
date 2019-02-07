f634bab60c695f06392f351abaa70bd5021f0b2b
Guillermo <memo33101@gmail.com>
2019-02-07 08:24:37 -0700

    Created List header file

---
+++ DataStructures/Model/Linear/List.hpp
@@ -0,0 +1,13 @@
+//
+//  List.hpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 2/7/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+
+#endif /* List_hpp */
