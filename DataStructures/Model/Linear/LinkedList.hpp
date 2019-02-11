7fe37a5c7cd0e24767bd29c239e298b927315cf4
Guillermo <memo33101@gmail.com>
2019-02-11 09:23:56 -0700

    Finished LinkedList class

--- DataStructures/Model/Linear/LinkedList.hpp
+++ DataStructures/Model/Linear/LinkedList.hpp
@@ -20,7 +20,182 @@ protected:
     LinearNode<Type> * end;
 private:
 public:
+    LinkedList();
+    virtual ~LinkedList();
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    bool contains(Type item);
 
 };
+
+template class<Type>
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
+template <class Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while (front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+template <class Type>
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode<Type> * newData =  new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNode(newData);
+    }
+    this->end = newData;
+    
+    this->size += 1;
+}
+template <class Type>
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index >=0 && index <= thiis->size);
+    if(index == this->size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if(index == 0)
+        {
+            toBeAdded->setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for(int positions = 0; position < index; position++)
+            {
+                previous = current;
+                current = current->getNextNode();
+            }
+            previous->setNextNode(toBeAdded);
+            toBeAdded->setNextNode(current);
+        }
+        this->size++;
+    }
+    
+}
+
+template <class Type>
+Type LinkedList<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < this->size);
+    Type data;
+    
+    LinearNode<Type> * current = front;
+    
+    for(int position = 0; position < index; position++)
+    {
+        current = current->getNextNode();
+    }
+    data = current getData();
+    
+    return data;
+}
+
+template <class Type>
+Type LinkedList<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < this->size);
+    
+    LinearNode<Type> * current = front;
+    LinearNode<Type> * toBeRemoved = nullptr;
+    LinearNode<Type> * previous = nullptr;
+    
+    Type removedData;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        this->front = this->front->getNextNode();
+    }
+    else
+    {
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNextNode();
+        }
+        toBeRemoved = current;
+        if(index == this->size - 1)
+        {
+            previous->setNextNode(nullptr);
+            end = previous;
+        }
+        else
+        {
+            current = toBeRemoved->getNextNode();
+            previous->setNextNode(current);
+        }
+        
+    }
+    this->size -= 1;
+    
+    removedData = toBeRemoved->getData();
+    delete toBeRemoved;
+    return removeData;
+}
+
+template <class Type>
+bool LinkedList<Type> :: contains(Type item)
+{
+    LinearNode<Type> * current = front;
+    bool isThere = false;
+    Type value;
+    for (int index = 0; index < this->size; index++)
+    {
+        value = current->getData();
+        if(value == item)
+        {
+            isThere = true;
+        }
+        current = current->getNextNode();
+    }
+    
+    return isThere;
+
+}
+
+template <class Type>
+LinearNode<Type> * LinkedList<Type> :: getEnd()
+{
+    return this->end;
+}
+template <class Type>
+LinearNode<Type> * LinkedList<Type> :: getFront()
+{
+    return this->front;
+}
+template <class Type>
+int LinkedList<Type> :: getSize() const
+{
+    return this->size;
+}
 
 #endif /* LinkedList_hpp */
