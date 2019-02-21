accc9607a73602fc3c463ef68da094305b4ba379
Guillermo <memo33101@gmail.com>
2019-02-21 08:09:37 -0700

    Created DoubleNode header file

---
+++ DataStructures/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,72 @@
+//
+//  DoubleNode.hpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 2/21/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+#include <iostream>
+#include "Node.hpp"
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+private:
+    DoubleNode<Type> * nextNode;
+    DoubleNode<Type> * previousNode;
+public:
+    //constructors
+    DoubleNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * nextNode, DoubleNode<Type> * previousNode);
+    //getters
+    DoubleNode<Type> * getNextNode();
+    DoubleNode<Type> * getPrevNode();
+    //setters
+    void setNextNode(DoubleNode<Type> * nextNode);
+    void setPrevNode(DoubleNode<Type> * previousNode);
+    
+};
+
+//constructor
+template<class Type>
+DoubleNode<Type> :: DoubleNode() : Node<Type>()
+{
+    
+}
+template<class Type>
+DoubleNode<Type> :: DoubleNode(Type data, DoubleNode<Type> * nextNode, DoubleNode<Type> * previousNode) : Node<Type>(data)
+{
+    this->nextNode = nextNode;
+    this->previousNode = previousNode;
+}
+//getters
+template<class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getNextNode()
+{
+    return this->nextNode;
+}
+template<class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getPreviousNode()
+{
+    return this->previousNode;
+}
+//setters
+template<class Type>
+void setNextNode(DoubleNode<Type> * nextNode)
+{
+    this->nextNode = nextNode;
+}
+template<class Type>
+void setPrevNode(DoubleNode<Type> * previousNode)
+{
+    this->previousNode = previousNode;
+}
+
+
+
+
+
+#endif /* DoubleNode_hpp */
