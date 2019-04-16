e82f893e6f74a5ba6b71869a153dfe4407c1745c
Guillermo <memo33101@gmail.com>
2019-04-16 08:14:49 -0600

    Finished tree abc

--- DataStructures/Model/Non-Linear/Tree.hpp
+++ DataStructures/Model/Non-Linear/Tree.hpp
@@ -17,7 +17,18 @@ protected:
     BinaryTreeNode<Type> * root;
 public:
     //informational methods
-    virtual int getSize()
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+    
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
 };
 
 
