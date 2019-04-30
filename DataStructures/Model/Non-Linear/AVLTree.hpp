41ee9ad30b7a2ebd7bbda654c90305a98a88cd51
Guillermo <memo33101@gmail.com>
2019-04-30 08:08:22 -0600

    Started AVLTree class

---
+++ DataStructures/Model/Non-Linear/AVLTree.hpp
@@ -0,0 +1,36 @@
+//
+//  AVLTree.hpp
+//  DataStructures
+//
+//  Created by Ramos aguirre, Guillermo on 4/30/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+}
+
+#endif /* AVLTree_hpp */
